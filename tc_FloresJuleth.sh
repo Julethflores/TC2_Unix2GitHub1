3942daefc3ab544b135f0df2bd94bf36f853e2ab
Juleth Flores <104948373+Julethflores@users.noreply.github.com>
2022-11-09 16:11:36 -0500

    Cambio realizado por Juleth Flores

---
+++ tc_FloresJuleth.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+echo -e "\n"
+echo "Aquí se escogieron las primeras 7 filas"
+echo -e "\n"
+head -n 7 EigenBetw.tsv
+echo -e "\n"
+echo "Aquí se escogieron las últimas 5 filas"
+tail -n 5 EigenBetw.tsv
+
